84c0fe82790c21b51e9a47e3b86d8cd04c1b457b
RuizoxDev <ruizox.hystic@gmail.com>
2023-02-01 11:34:59 +0100

    reset and first progress

--- script.js
+++
@@ -1,7 +0,0 @@
-fetch('https://api.mcsrvstat.us/2/virtualhit.es')
-    .then(response => response.json())
-    .then(data => {
-        let usersOnline = data.players.online;
-        let link = document.querySelector("a.counter-link");
-        link.innerHTML = `Users online: ${usersOnline}`
-    });
